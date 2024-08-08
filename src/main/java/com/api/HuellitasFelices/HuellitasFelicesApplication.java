1e92c661bbcf20c8f328e594719774bb65a933d6
Kevvyn <kevinstevenperdomogomez842@gmail.com>
2024-08-07 22:52:48 -0500

    Se agrega el proyecto al repositorio

---
+++ src/main/java/com/api/HuellitasFelices/HuellitasFelicesApplication.java
@@ -0,0 +1,13 @@
+package com.api.HuellitasFelices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuellitasFelicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuellitasFelicesApplication.class, args);
+	}
+
+}
