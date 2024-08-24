8c476bbdd08258efcb097156c5d9c30fc5997a3f
Kevvyn <kevinstevenperdomogomez842@gmail.com>
2024-08-23 20:24:16 -0500

    Se crea los metodos saveUser y getById en las capas de UserService y UserController

--- src/test/java/com/api/HuellitasFelices/controllers/UserController.java
+++ src/test/java/com/api/HuellitasFelices/controllers/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+//RestController simplifica la creación de servicios web RESTful, permitiendo que los métodos dentro del controlador gestionen solicitudes HTTP
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -20,5 +22,12 @@ public class UserController {
     }
 
     @PostMapping
-    public UserModel saveUser(@RequestBody)
+    public UserModel saveUser(@RequestBody UserModel user ){
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable Long id){
+        return this.userService.getById(id);
+    }
  }
--- src/test/java/com/api/HuellitasFelices/services/UserService.java
+++ src/test/java/com/api/HuellitasFelices/services/UserService.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+//La etiqueta @Service sirve para marcar la clase que se encargara de la logica del negocio
 @Service
 public class UserService {
 
+    //este anotation es para la inyeccion de dependencias
     @Autowired
     IUserRepository userRepository;
 
@@ -19,5 +22,10 @@ public class UserService {
 
     public UserModel saveUser(UserModel user){
         return userRepository.save(user);
+    }
+
+    //Metodo para buscar una persona con su id
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
     }
 }
